5baf34c1151a476a80ffadc65984bead106d3ac1
Margarita <00225117@uca.edu.sv>
2018-06-18 22:45:11 -0600

    terminando clase Modelo

--- src/modelo/Filtro.java
+++ src/modelo/Filtro.java
@@ -10,53 +10,84 @@ package modelo;
  * @author LN710Q
  */
 public class Filtro {
-    private int id;
+   private int id;
     private String codigo;
     private String marca;
     private int stock;
     private boolean existencia;
     
-     public Filtro(){
-         
-     }      
+    public Filtro(){}
 
     public Filtro(int id, String codigo, String marca, int stock, boolean existencia) {
         this.id = id;
+        this.codigo = codigo;
         this.marca = marca;
         this.stock = stock;
         this.existencia = existencia;
-        this.codigo = codigo;
     }
     
     public Filtro(String codigo, String marca, int stock, boolean existencia) {
-        
         this.codigo = codigo;
         this.marca = marca;
         this.stock = stock;
         this.existencia = existencia;
-        
     }
+
     public Filtro(String marca, int stock, boolean existencia) {
-   
         this.marca = marca;
         this.stock = stock;
         this.existencia = existencia;
     }
 
-    public String getMarca() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public int getId() {
+        return id;
     }
 
-    public boolean getExistencia() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getCodigo() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return codigo;
     }
 
-    public int getStook() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
     }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public boolean isExistencia() {
+        return existencia;
+    }
+
+    public void setExistencia(boolean existencia) {
+        this.existencia = existencia;
+    }
+    /**
+     * Al generar los Setters y Getters automaticamente, no me generaba el de getExistencia 
+     * es cual se usa en las clases FilstroDao.java y Consulta.java
+     * y el (return existencia;) dentro del getExistencia(){ 
+     * lo tuve que hacer manualmente
+     * @return 
+     */
+    public boolean getExistencia() {
+        return existencia;
+    }
+    
      
 }
